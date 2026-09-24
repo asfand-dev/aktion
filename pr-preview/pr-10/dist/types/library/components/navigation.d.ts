aea6e3d7e303fdb872352d30d92fa78ff37fa76e
Primajin <Primajin@users.noreply.github.com>
2026-09-24 12:51:40 +0000

    Remove preview for PR  🛬

--- pr-preview/pr-10/dist/types/library/components/navigation.d.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentSpec } from '../types.js';
-export declare const BreadcrumbItem: ComponentSpec;
-export declare const Breadcrumb: ComponentSpec;
-export declare const Pagination: ComponentSpec;
-export declare const NavbarItem: ComponentSpec;
-export declare const Navbar: ComponentSpec;
